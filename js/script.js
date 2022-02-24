c7ab2d449862359b9f025fa40f47a1e5d8263a70
Maxim Butenko <maxbuten97@yandex.ru>
2022-02-24 23:30:43 +0700

    use fetch and promise

--- js/script.js
+++ js/script.js
@@ -247,33 +247,37 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            
 
-             request.setRequestHeader('Content-type', 'application/json');
+             
             const formData = new FormData(form);
 
-            const object = {};
+             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
 
 
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                 } else {
-                     showThanksModal(message.failure); 
-                    }
-            }); 
+            fetch('server.php', {
+                method: "POST",
+                 headers: {
+                    'Content-type': 'application/json'
+                }, 
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                 showThanksModal(message.success);
+                 
+                 statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
           });
       }
 
@@ -301,6 +305,18 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
             closeModal();
           }, 4000);
       }
+
+      
+
+
+
+
+
+
+
+
+
+
 });
 
 
